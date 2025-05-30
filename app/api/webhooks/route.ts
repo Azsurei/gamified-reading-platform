f17d5cb23d27e2a5f00a3008cf5629b6f6cbf9c3
Azsurei <a20203298@pucp.edu.pe>
2025-05-29 19:10:50 -0500

    solucionando nombres y eventos de weebhooks

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -2,7 +2,7 @@
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 import { db } from "@/db/drizzle";
-import { usuario, desafio, progresoDesafio } from "@/db/schema";
+import { usuario, desafio, progresoDesafio, respuesta, lecturaCompletada, comodinUsuario, lecturaDesafioLog, comodinLecturaUsuario} from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export async function POST(req: NextRequest) {
@@ -47,8 +47,40 @@ export async function POST(req: NextRequest) {
         );
       }
     } else if (eventType === "user.deleted") {
+      console.log(`🧹 Eliminando datos relacionados al usuario ${id}`);
+
+      await db.delete(respuesta).where(eq(respuesta.usuarioId, id));
+      await db.delete(progresoDesafio).where(eq(progresoDesafio.usuarioId, id));
+      await db
+        .delete(lecturaCompletada)
+        .where(eq(lecturaCompletada.usuarioId, id));
+      await db.delete(comodinUsuario).where(eq(comodinUsuario.usuarioId, id));
+      await db
+        .delete(lecturaDesafioLog)
+        .where(eq(lecturaDesafioLog.usuarioId, id));
+      await db
+        .delete(comodinLecturaUsuario)
+        .where(eq(comodinLecturaUsuario.usuarioId, id));
+
+      // Eliminar finalmente el usuario
       await db.delete(usuario).where(eq(usuario.id, id));
-      console.log(`🗑️ Usuario eliminado de DB: ${id}`);
+
+      console.log(`🗑️ Usuario y datos relacionados eliminados: ${id}`);
+    } else if (eventType === "user.updated") {
+      const user = evt.data;
+
+      const usuarioActualizado = {
+        nombre: `${user.first_name} ${user.last_name}`.trim(),
+        correo: user.email_addresses[0]?.email_address ?? "",
+        imagen: user.image_url ?? null,
+      };
+
+      await db
+        .update(usuario)
+        .set(usuarioActualizado)
+        .where(eq(usuario.id, user.id));
+
+      console.log(`🔄 Usuario actualizado: ${user.id}`, usuarioActualizado);
     }
 
     return new Response("Webhook recibido", { status: 200 });
